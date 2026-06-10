0fc36754540b0e741d1ffe4bbaaf300efa4b7e60
Kartik Tulsian <kartiktulsian0517@gmail.com>
2026-06-10 18:24:19 +0530

    update in date

--- src/components/Countdown/CountDown.tsx
+++ src/components/Countdown/CountDown.tsx
@@ -19,7 +19,7 @@ export default function CountDown() {
         <ImpDates />
 
         {/* Timer with built-in spacing */}
-        <Timer launchDate="2026-06-10T23:59:59" />
+        <Timer launchDate="2026-06-23T23:59:59" />
       </div>
     </section>
 
--- src/components/Countdown/ImpDates.tsx
+++ src/components/Countdown/ImpDates.tsx
@@ -101,8 +101,8 @@ export default function ImpDates() {
         <div className="overflow-x-auto:hidden rounded-md border border-gray-200 shadow-sm">
           <CreateTable
             datas={[
-              { col1: 'Full Paper Submission (Extended):', col2: '10th June 2026' },
-              { col1: 'Acceptance Notification:', col2: '20th June 2026' },
+              { col1: 'Full Paper Submission (Extended):', col2: '25th June 2026' },
+              // { col1: 'Acceptance Notification:', col2: '20th June 2026' },
               { col1: 'Deadline for Paper Registration:', col2: '30th June 2026' },
               { col1: 'Final/Camera-ready Paper Submission:', col2: '10th July 2026' },
               { col1: 'Presentation Submission:', col2: '31st July 2026' },
